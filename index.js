a8fe344e9bb55aa50cb7207aea8b3d20f880abd0
secured-jenkins <136594066+secured-jenkins@users.noreply.github.com>
2023-12-03 09:46:31 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,50 @@
+import express from "express";
+import bodyParser from "body-parser";
+import fetch from "node-fetch";
+const app = express();
+const port = process.env.PORT || 8085;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.json({
+    availablePaths: [
+      {
+        path: "/tc-proxy",
+        operation: "POST",
+        description:
+          "Forwards requests to Trust Commerce and appends the CORS header to the response, then passes the response back to you.",
+      },
+    ],
+  });
+});
+
+app.post("/tc-proxy", async (req, res) => {
+  const requestBody = new URLSearchParams(req.body).toString();
+  console.log(requestBody);
+
+  const targetURL = "https://vault.trustcommerce.com/trans/?";
+
+  const response = await fetch(targetURL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    body: requestBody,
+  });
+
+  const responseData = await response.text();
+
+  // Set the Access-Control-Allow-Origin header to allow all origins
+  res.setHeader("Access-Control-Allow-Origin", "*");
+
+  res.status(response.status).send(responseData);
+});
+
+app.use((req, res) => {
+  res.status(404).send("This path does not exist.");
+});
+
+app.listen(port, () => {
+  console.log("Listening");
+});
