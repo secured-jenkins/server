6a37f662345eeff4f829c0e8c2fdba53d67dca0b
secured-jenkins <136594066+secured-jenkins@users.noreply.github.com>
2023-12-03 12:32:24 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,9 +5,12 @@ const app = express();
 const port = process.env.PORT || 8085;
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors({
+  origin: '*',
+}));
 
 app.get("/", (req, res) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  // res.setHeader('Access-Control-Allow-Origin', '*');
   res.json({
     availablePaths: [
       {
@@ -37,13 +40,13 @@ app.post("/tc-proxy", async (req, res) => {
   const responseData = await response.text();
 
   // Set the Access-Control-Allow-Origin header to allow all origins
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  // res.setHeader("Access-Control-Allow-Origin", "*");
 
   res.status(response.status).send(responseData);
 });
 
 app.use((req, res) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  // res.setHeader('Access-Control-Allow-Origin', '*');
   res.status(404).send("This path does not exist.");
 });
 
